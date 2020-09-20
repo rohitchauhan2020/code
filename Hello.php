2f764a2e7feac7c5f44959a821af069371dc3f96
Rohit <nrc.rohitrana@gmail.com>
2020-09-20 10:02:24 +0530

    print hello in this file aweee

---
+++ Hello.php
@@ -0,0 +1,30 @@
+<?php 
+  //echo "hello this is my first programm <br \>";
+  //echo 8*8 . '<br \>';
+
+  $a = 5; //integer
+  $b = 5.5; // float
+  $c = "hello world";
+
+  $c = [1,2,3,45,6]; // array(1,3,4);
+
+  //print_r($c[1]);
+
+  $ct = [
+      'name' => 'rohit',
+      'vill' => 'puranpur'
+  ]; // array(1,3,4);
+
+  //print_r($ct);
+
+  //print_r($ct['name']);
+
+  $d = (object) $ct;
+
+  print_r($d->name);
+
+
+   
+
+
+?>

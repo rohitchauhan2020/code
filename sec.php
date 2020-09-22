11d25e8eaa9c289280da9b8f4af496aa858968c0
rohitchauhan2020 <me.rohitchauhan@gmail.com>
2020-09-22 12:17:15 +0530

    this edit is in another git repository

---
+++ sec.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body bgcolor="lightblue" text="white" >
+    
+
+
+<?php
+echo "<p ><i>Hi r u looking for google</i></p>";
+
+?>
+
+<a href="https://www.google.com/"> <u><marquee width="100%" direction="left" behavior="alternate" ><span style="font-size:38px;font-weight:bold;color:#1F7A8C;font-family:'lobster', cursive;">click here</span>
+        </marquee> </u></a>
+</body>
+</html>
